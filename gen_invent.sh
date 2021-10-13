6bc5cc51deab7233d0e74433cb6bfc7d2f8cc40f
drmdvorskiy <drm.dvorskiy@gmail.com>
2021-10-13 23:10:08 +0300

    ver2

--- gen_invent.sh
+++ gen_invent.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+arr=(`grep -Po "(\d+\.){3}\d+" my.invent | cut -d "." -f 4`)
+
 
 for i in {1..254}; do
-  if [ `grep 192.168.6.${i} my.invent|wc -l` -eq 0 ]; then
-    echo "VRRP_IP is 192.168.6.${i}"; exit 0
+  if [ `echo "${arr[@]}" | tr " " "\n" | grep "^${i}$" | wc -l` -ne 0 ]; then
+    echo "VRRP=192.168.6.${i}"
+    break
   fi
 done
