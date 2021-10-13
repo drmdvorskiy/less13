fa921ea48c3dce81f31d0ed3f31570c39e1e7a92
drmdvorskiy <drm.dvorskiy@gmail.com>
2021-10-13 23:12:02 +0300

    ver2.1

--- gen_invent.sh
+++ gen_invent.sh
@@ -3,7 +3,7 @@ arr=(`grep -Po "(\d+\.){3}\d+" my.invent | cut -d "." -f 4`)
 
 
 for i in {1..254}; do
-  if [ `echo "${arr[@]}" | tr " " "\n" | grep "^${i}$" | wc -l` -ne 0 ]; then
+  if [ `echo "${arr[@]}" | tr " " "\n" | grep "^${i}$" | wc -l` -eq 0 ]; then
     echo "VRRP=192.168.6.${i}"
     break
   fi
